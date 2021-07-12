0e8024fe8c3e76e2ef4aaac44884fe326e1d404a
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-07-12 16:41:01 -0300

    adiciona requisito 2

---
+++ src/components/ProductList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProductList extends React.Component {
+  render() {
+    return (
+      <p data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+    );
+  }
+}
+
+export default ProductList;
