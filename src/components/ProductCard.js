1adecaf6c4fc1ee6728e6a21af3ac10d07b02db8
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-07-12 19:26:37 -0300

    Implementa requisito 5.

---
+++ src/components/ProductCard.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ProductCard extends Component {
+  render() {
+    const { product } = this.props;
+    const { title, price, thumbnail } = product;
+    return (
+      <div data-testid="product">
+        <h4>{ title }</h4>
+        <img src={ thumbnail } alt={ title } />
+        <p>{ price }</p>
+      </div>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    price: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default ProductCard;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,65 @@
 import React from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import ProductCard from './ProductCard';
 
 class ProductList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+      searchInput: '',
+    };
+    this.inputSearchChange = this.inputSearchChange.bind(this);
+    this.searchButton = this.searchButton.bind(this);
+  }
+
+  inputSearchChange(event) {
+    const { value } = event.target;
+    this.setState({
+      searchInput: value,
+    });
+  }
+
+  async searchButton() {
+    const { searchInput } = this.state;
+    const filter = await getProductsFromCategoryAndQuery('categoryId', searchInput);
+    this.setState({
+      products: filter.results,
+      searchInput: '',
+    });
+  }
+
   render() {
+    const { products, searchInput } = this.state;
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <div>
+        <form>
+          <label htmlFor="searchProduct">
+            <input
+              type="text"
+              name="searchProduct"
+              value={ searchInput }
+              data-testid="query-input"
+              onChange={ this.inputSearchChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={ this.searchButton }
+          >
+            Procurar
+          </button>
+        </form>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <div>
+          { products.map((product) => (
+            <ProductCard key={ product.title } product={ product } />
+          ))}
+        </div>
+      </div>
     );
   }
 }
