3bf9b836d59eea54bf082a2de854d07b1412a70b
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-07-12 16:12:03 -0300

    adiciona requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
-// const fetch = require('node-fetch');
-
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const responseJson = response.json();
+  return responseJson;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const responseJson = response.json();
+  return responseJson;
 }
